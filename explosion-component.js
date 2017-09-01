ed5352993b29f7cdaae8fe592a2dff466a494730
Anwesh43 <anweshthecool0@gmail.com>
2017-09-01 23:22:40 +0530

    adding exploision class

--- explosion-component.js
+++ explosion-component.js
@@ -19,3 +19,31 @@ class ExplosionComponent extends HTMLElement {
         this.render()
     }
 }
+class Explosion {
+    constructor(x,y) {
+        this.x = x
+        this.y = y
+        this.maxR = Math.min(w,h)/16
+        this.r = 0
+    }
+    draw(context) {
+        context.save()
+        context.translate(this.x,this.y)
+        var deg = 3.6
+        context.fillStyle = '#d32f2f'
+        for(var i = 0;i<100;i++) {
+            const x = this.r*Math.cos(deg*Math.PI/180),y = this.r*Math.sin(deg*Math.PI/180)
+            context.beginPath()
+            context.arc(x,y,r,0,2*Math.PI)
+            context.fill()
+            deg += 3.6
+        }
+        context.restore()
+    }
+    update() {
+        this.r += this.maxR/10
+    }
+    stopped() {
+        return this.r > this.maxR
+    }
+}
